6e003a6e23ebac894c17afe8b584f9cc63f9b690
keithtyser <keithtyser@gmail.com>
2025-06-30 12:27:44 -0400

    finished section 5: multipage applications

--- 27_multipage/starter.py
+++ 27_multipage/starter.py
@@ -1,12 +1,27 @@
 import streamlit as st
 
 # Set page configuration
+st.set_page_config(
+    page_title='Homepage',
+    page_icon="📈",
+    layout="centered",
+    initial_sidebar_state="auto"
+)
 
 # Initialize a sample df and store it in the session state
+df = pd.DataFrame({"col1": [1,2,3],
+                    "col2": [4,5,6]})
+
+if "df" not in st.session_state:
+    st.session_state['df'] = df
 
 # Initialize state with the key "product" set to 0
+if "product" not in st.session_state:
+    st.session_state['product'] = 0
 
 # Define a function to multiply two numbers
+def multiply(x1, x2):
+    st.session_state['product'] = x1 * x2
 
 if __name__ == "__main__":
 
@@ -19,6 +34,7 @@ if __name__ == "__main__":
     with col2:
         x2 = st.number_input("Pick another number", 0, 10, key="x2")
 
-    st.button("Multiply!", type="primary")
+    st.button("Multiply!", type="primary", on_click=multiply, args=((x1,x2)))
 
 st.write(st.session_state)
+
--- 29_multipage_refactor/starter.py
+++ 29_multipage_refactor/starter.py
@@ -7,7 +7,9 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-if "product" not in st.session_state:
+if all(key not in st.session_state.keys() for key in ('product', 'x1', 'x2')):
+    st.session_state['x1'] = 0
+    st.session_state['x2'] = 0
     st.session_state['product'] = 0
 
 
@@ -17,6 +19,10 @@ def multiply(x1, x2):
 # Workaround from: https://stackoverflow.com/questions/74968179/session-state-is-reset-in-streamlit-multipage-app
 
 # Function to keep a value (copy from temporary to permanent)
+def keep(key):
+    st.session_state[key] = st.session_state[f"_{key}"]
+def unkeep(key):
+    st.session_state[f"_{key}"] = st.session_state[key]
 
 # Function to reassign value to temporary key (copy from permanent to temporary)
 
@@ -28,9 +34,9 @@ if __name__ == "__main__":
     col1, col2 = st.columns(2)
 
     with col1:
-        x1 = st.number_input("Pick a number", 0, 10, key="x1")
+        x1 = st.number_input("Pick a number", 0, 10, key="_x1", on_change=keep, args(("x1",)))
     with col2:
-        x2 = st.number_input("Pick another number", 0, 10, key='x2')
+        x2 = st.number_input("Pick another number", 0, 10, key='_x2', on_change=keep, args(("x2",)))
 
     st.button("Multiply!", type="primary", on_click=multiply, args=((x1, x2)))
 
